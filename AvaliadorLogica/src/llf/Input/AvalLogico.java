693a71bc63786fb6dca446b1c26d8c1cf23c4796
Lucas Lucena Falbo <95.lucas@gmail.com>
2014-12-01 23:33:31 -0300

    Finalizado a manipulação de dados (leitura e gravação)

--- AvaliadorLogica/src/llf/Input/AvalLogico.java
+++ AvaliadorLogica/src/llf/Input/AvalLogico.java
@@ -1,6 +1,10 @@
 package llf.Input;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +21,12 @@ public class AvalLogico {
 			Object[] ex = lerArquivo();
 			System.out.println(ex[1]);
 			
+			String[] test = new String[2];
+			test[0] = "You're dumb";
+			
+			fecharArquivo(test);
+			
+			
 		} catch (Exception e){
 			System.out.println(e.getMessage());
 		}
@@ -29,13 +39,13 @@ public class AvalLogico {
 	public static Object[] lerArquivo() throws IOException{
 		
 		//Seta o tipo de chars
-		Charset ct = Charset.forName("US-ASCII");
+		Charset ct = Charset.forName("utf-8");
 		
 		//Cria um lista para adicionar os termos
 		List<String> ex = new ArrayList<String>();
 		
 		//Um for que varre todo o arquivo
-		for (String line : Files.readAllLines(Paths.get(System.getProperty("user.dir") + "/src/llf/Input/Expressao.in"), ct)) {
+		for (String line : Files.readAllLines(Paths.get("Expressao.in"), ct)) {
 				ex.add(line);
 		
 		}
@@ -43,6 +53,21 @@ public class AvalLogico {
 		//Retorna um array com todas as expressoes
 		return ex.toArray();
 		
+	}
+	
+	//Escreve mudanças para o arquivo
+	public static void fecharArquivo(String[] ex) throws IOException{
+		
+		//Inicializa novo objeto que registra no arquivo especificado
+		Writer autor = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("Expressao.out"), "utf-8"));
+		
+		//Um for para pegar todos os itens do array
+		for(int i = 0; i< ex.length; i++){
+			autor.write((String) ex[i] + "\n");
+		}
+		
+		//fecha o objeto
+		autor.close();
 	}
 	
 
