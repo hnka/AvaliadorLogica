3c32a9b37dafc1090f52fc8562e340ef4d0fc0e0
Lucas Lucena Falbo <95.lucas@gmail.com>
2014-12-01 23:07:57 -0300

    Manipulação de arquivos especificamente leitura

--- AvaliadorLogica/src/llf/Input/AvalLogico.java
+++ AvaliadorLogica/src/llf/Input/AvalLogico.java
@@ -1,11 +1,49 @@
 package llf.Input;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 public class AvalLogico {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		
-		System.out.print("Hello World!");
+		
+		try {			
+			
+			Object[] ex = lerArquivo();
+			System.out.println(ex[1]);
+			
+		} catch (Exception e){
+			System.out.println(e.getMessage());
+		}
 
 	}
+	
+	
+	
+	//Leitura do Arquivo
+	public static Object[] lerArquivo() throws IOException{
+		
+		//Seta o tipo de chars
+		Charset ct = Charset.forName("US-ASCII");
+		
+		//Cria um lista para adicionar os termos
+		List<String> ex = new ArrayList<String>();
+		
+		//Um for que varre todo o arquivo
+		for (String line : Files.readAllLines(Paths.get(System.getProperty("user.dir") + "/src/llf/Input/Expressao.in"), ct)) {
+				ex.add(line);
+		
+		}
+		
+		//Retorna um array com todas as expressoes
+		return ex.toArray();
+		
+	}
+	
 
 }
