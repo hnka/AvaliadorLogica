ce4fa33db256c99c3553dcc02e76d93c530aa817
Cecília Hunka <hnkcecilia@users.noreply.github.com>
2014-12-02 02:19:05 -0300

    Update AvaliadorExpressoes.java
    Criacao do metodo alturaArvore, falta integrar com o tipo de dado que sera passado pelo main, e organizar o metodo de forma recursiva (eu to com muito sono para entender o que eu to fazendo, so sei que o raciocinio e por ai :D)

--- AvaliadorLogica/src/expressoes/AvaliadorExpressoes.java
+++ AvaliadorLogica/src/expressoes/AvaliadorExpressoes.java
@@ -1,5 +1,60 @@
 package expressoes;
 
 public class AvaliadorExpressoes {
+  
+  	private String atomX;
+	private String atomY;
+	private String atomZ;
+	
+	public AvaliadorExpressoes() {
+
+		this.atomX = "x";
+		this.atomX = "y";
+		this.atomX = "z";
+		
+	}
+	
+	//create method to receive two strings as arguments and organize the returns
+	public int alturaArvore(String expressao) {
+		
+		int altura = 0;
+		
+		if(expressao == this.atomX || expressao == this.atomY || expressao == this.atomZ) {
+			
+			return altura;
+			
+		} else {
+			
+			//regex matching (-
+			if(expressao.matches("^([\\(])(-)")) {
+				
+				altura = altura + 1;
+				return this.alturaArvore(expressao.substring(2, expressao.length()-1));
+				
+			//regex matching the first occurence of () any symbol ()
+			} else if (expressao.matches("([\\(])(.*)([\\)])(\\W)([\\(])(.*)([\\)])")) {
+				
+				altura = altura + 1;
+				String[] subs = expressao.split("([\\(])(.*)([\\)])(\\W)([\\(])(.*)([\\)])", 1);
+	
+				int altura1 = this.alturaArvore(subs[0]);
+				int altura2 = this.alturaArvore(subs[1]);
+				
+				if(altura1 < altura2) {
+					
+					return altura2;
+					
+				} else {
+					
+					return altura1;
+					
+				}
+				
+			}
+
+		}
+		return altura;
+		
+	}
 
 }
