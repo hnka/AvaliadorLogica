92b6299ac887ca7e1f6840715ee596df9accdd2b
Cecília Hunka <hnkcecilia@users.noreply.github.com>
2014-12-06 01:33:52 -0300

    Implementacao do calculo da quantidade de expressoes, ja eliminando as duplicidades e limpeza dos print do metodo do calculo da altura da arvore.

--- AvaliadorLogica/src/expressoes/AvaliadorExpressoes.java
+++ AvaliadorLogica/src/expressoes/AvaliadorExpressoes.java
@@ -23,8 +23,6 @@ public class AvaliadorExpressoes {
 		
 		//base: regex for atoms
 		if(expressao.matches("[xyz]")) {
-			
-			System.out.println(expressao);
 
 			altura = altura + 0;
 			
@@ -36,7 +34,6 @@ public class AvaliadorExpressoes {
 				altura = altura + 1;
 				
 				String temp = expressao.substring(2, 3);
-				System.out.println(temp);
 				
 				int alturaTemp = this.alturaArvore(temp);
 				
@@ -48,9 +45,7 @@ public class AvaliadorExpressoes {
 				altura = altura + 1;
 				
 				String temp = expressao.substring(2, expressao.length()-1);
-				System.out.println(temp);
 				
-				//observar comportamento, se a altura esta sendo aumentada
 				int alturaTemp = this.alturaArvore(temp);
 				
 				altura = altura + alturaTemp;
@@ -62,9 +57,7 @@ public class AvaliadorExpressoes {
 				altura = altura + 1;
 
 				String temp1 = expressao.substring(1,2);
-				System.out.println(temp1);
 				String temp2 = expressao.substring(3,4);
-				System.out.println(temp2);
 				
 				int altura1 = this.alturaArvore(temp1);
 				int altura2 = this.alturaArvore(temp2);
@@ -72,16 +65,14 @@ public class AvaliadorExpressoes {
 				if(altura1 < altura2) {
 					
 					altura = altura + altura2;
-					System.out.println(altura);
 					
 				} else {
 					
 					altura = altura + altura1;
-					System.out.println(altura);
 					
 				}
 			
-			//case 4: regex for (atom symbol subexpression)
+			//case 4: regex for (atom symbol (subexpression))
 			} else if(expressao.matches("[\\(][xyz][^xyz01][\\(].*[\\)][\\)]")) {
 				
 				altura = altura + 1;
@@ -99,9 +90,7 @@ public class AvaliadorExpressoes {
 				}
 				
 				String sub1 = expressao.substring(1,2);
-				System.out.println(sub1);
 				String sub2 = expressao.substring(trim-1,expressao.length()-1);
-				System.out.println(sub2);
 				
 				int altura1 = this.alturaArvore(sub1);
 				int altura2 = this.alturaArvore(sub2);
@@ -109,16 +98,14 @@ public class AvaliadorExpressoes {
 				if(altura1 < altura2) {
 					
 					altura = altura + altura2;
-					System.out.println(altura);
 					
 				} else {
 					
 					altura = altura + altura1;
-					System.out.println(altura);
 					
 				}
 			
-			//case 5: regex for (subexpression symbol atom)
+			//case 5: regex for ((subexpression) symbol atom)
 			} else if (expressao.matches("[\\(][\\(].*[\\)][^xyz01][xyz][\\)]")) {
 				
 				altura = altura + 1;
@@ -131,16 +118,12 @@ public class AvaliadorExpressoes {
 				
 				if(matcher.find()) {
 					
-					System.out.println(matcher.start());
-					System.out.println(matcher.end());
 					trim = matcher.end();
 					
 				}
 				
 				String sub1 = expressao.substring(1,trim-1);
-				System.out.println(sub1);
 				String sub2 = expressao.substring(trim,expressao.length()-1);
-				System.out.println(sub2);
 				
 				int altura1 = this.alturaArvore(sub1);
 				int altura2 = this.alturaArvore(sub2);
@@ -148,19 +131,15 @@ public class AvaliadorExpressoes {
 				if(altura1 < altura2) {
 					
 					altura = altura + altura2;
-					System.out.println(altura);
 					
 				} else {
 					
 					altura = altura + altura1;
-					System.out.println(altura);
 					
 				}
 				
-			//case 6: regex for (subexpression symbol subexpression)
+			//case 6: regex for ((subexpression) symbol (subexpression))
 			} else if(expressao.matches("[\\(][\\(].*[\\)][^xyz01][\\(].*[\\)][\\)]")) {
-				
-				System.out.println("achei");
 				
 				altura = altura + 1;
 				
@@ -179,9 +158,6 @@ public class AvaliadorExpressoes {
 				String sub1 = expressao.substring(1, trim-2);
 				String sub2 = expressao.substring(trim-1, expressao.length()-1);
 				
-				System.out.println(sub1);
-				System.out.println(sub2);
-				
 				int altura1 = this.alturaArvore(sub1);				
 				int altura2 = this.alturaArvore(sub2);
 
@@ -189,12 +165,10 @@ public class AvaliadorExpressoes {
 				if(altura1 < altura2) {
 					
 					altura = altura + altura2;
-					System.out.println(altura);
 					
 				} else {
 					
 					altura = altura + altura1;
-					System.out.println(altura);
 					
 				}
 				
@@ -204,6 +178,168 @@ public class AvaliadorExpressoes {
 		
 		return altura;
 		
+	}
+	
+	public int quantidadeSubexpressoes(String expressao) {
+		
+		int quantidadeSubs = 0;
+		
+		//base: regex for atoms
+		if(expressao.matches("[xyz]")) {
+
+			quantidadeSubs = quantidadeSubs + 1;
+			
+		} else {
+			
+			//case 1: regex for (-atom)
+			if(expressao.matches("[\\(][-][xyz][\\)]")) {
+				
+				quantidadeSubs = quantidadeSubs + 1;
+				
+				String temp = expressao.substring(2, 3);
+				
+				int subsTemp = this.quantidadeSubexpressoes(temp);
+				
+				quantidadeSubs = quantidadeSubs + subsTemp;
+			
+			//case 2: regex for (-(subexpression))
+			} else if(expressao.matches("[\\(][-][\\(].*[\\)][\\)]")) {
+				
+				quantidadeSubs = quantidadeSubs + 1;
+				
+				String temp = expressao.substring(2, expressao.length()-1);
+				
+				int subsTemp = this.quantidadeSubexpressoes(temp);
+				
+				quantidadeSubs = quantidadeSubs + subsTemp;
+				
+			
+			//case 3: regex for (atom symbol atom)
+			} else if(expressao.matches("[\\(][xyz][^xyz01][xyz][\\)]")) {
+				
+				quantidadeSubs = quantidadeSubs + 1;
+
+				String sub1 = expressao.substring(1,2);
+				String sub2 = expressao.substring(3,4);
+				
+				if(sub1.equals(sub2)) {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					quantidadeSubs = quantidadeSubs + quantidade1;
+					
+				} else {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					int quantidade2 = this.quantidadeSubexpressoes(sub2);
+					quantidadeSubs = quantidadeSubs + quantidade1 + quantidade2;
+					
+				}
+	
+			//case 4: regex for (atom symbol (subexpression))
+			} else if(expressao.matches("[\\(][xyz][^xyz01][\\(].*[\\)][\\)]")) {
+				
+				quantidadeSubs = quantidadeSubs + 1;
+				
+				String regex = "[\\(][xyz][^xyz01][\\(]";
+				Pattern pattern = Pattern.compile(regex);
+				Matcher matcher = pattern.matcher(expressao);
+				
+				int trim = 0;
+				
+				if(matcher.find()) {
+					
+					trim = matcher.end();
+					
+				}
+				
+				String sub1 = expressao.substring(1,2);
+				String sub2 = expressao.substring(trim-1,expressao.length()-1);
+				
+				if(sub1.equals(sub2)) {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					quantidadeSubs = quantidadeSubs + quantidade1;
+					
+				} else {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					int quantidade2 = this.quantidadeSubexpressoes(sub2);
+					quantidadeSubs = quantidadeSubs + quantidade1 + quantidade2;
+					
+				}
+			
+			//case 5: regex for ((subexpression) symbol atom)
+			} else if (expressao.matches("[\\(][\\(].*[\\)][^xyz01][xyz][\\)]")) {
+				
+				quantidadeSubs = quantidadeSubs + 1;
+				
+				String regex = "[\\(][\\(].*[\\)][^xyz01]";
+				Pattern pattern = Pattern.compile(regex);
+				Matcher matcher = pattern.matcher(expressao);
+				
+				int trim = 0;
+				
+				if(matcher.find()) {
+
+					trim = matcher.end();
+					
+				}
+				
+				String sub1 = expressao.substring(1,trim-1);
+				String sub2 = expressao.substring(trim,expressao.length()-1);
+				
+				if(sub1.equals(sub2)) {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					quantidadeSubs = quantidadeSubs + quantidade1;
+					
+				} else {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					int quantidade2 = this.quantidadeSubexpressoes(sub2);
+					quantidadeSubs = quantidadeSubs + quantidade1 + quantidade2;
+					
+				}
+				
+			//case 6: regex for ((subexpression) symbol (subexpression))
+			} else if(expressao.matches("[\\(][\\(].*[\\)][^xyz01][\\(].*[\\)][\\)]")) {
+				
+				quantidadeSubs = quantidadeSubs + 1;
+				
+				String regex = "[\\(][\\(].*[\\)][^xyz01][\\(]";
+				Pattern pattern = Pattern.compile(regex);
+				Matcher matcher = pattern.matcher(expressao);
+				
+				int trim = 0;
+				
+				if(matcher.find()) {
+					
+					trim = matcher.end();
+					
+				}
+				
+				String sub1 = expressao.substring(1, trim-2);
+				String sub2 = expressao.substring(trim-1, expressao.length()-1);
+
+				if(sub1.equals(sub2)) {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					quantidadeSubs = quantidadeSubs + quantidade1;
+					
+				} else {
+					
+					int quantidade1 = this.quantidadeSubexpressoes(sub1);
+					int quantidade2 = this.quantidadeSubexpressoes(sub2);
+					quantidadeSubs = quantidadeSubs + quantidade1 + quantidade2;
+					
+				}
+				
+			}
+
+		}
+		
+		return quantidadeSubs;
+			
 	}
 
 }
